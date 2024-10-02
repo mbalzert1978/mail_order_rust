5a277011093097782962e71f211e793c3cf2495b
mbalzert1978 <teilen-01-koralle@icloud.com>
2024-10-02 17:38:30 +0200

    constants

---
+++ src/constants.rs
@@ -0,0 +1,14 @@
+pub const SOURCE: &str = "M:/Documents/familie/Markus/scans";
+pub const TARGET: &str = "M:/Documents/familie/Markus/Briefverkehr";
+pub const DB_NAME: &str = "briefverkehr.db";
+
+pub const READ_DIR_ERROR: &str = "Failed to read directory: ";
+
+pub const VALID_DAYS: u16 = 31;
+pub const VALID_MONTHS: u16 = 12;
+pub const VALID_YEARS: u16 = 2099;
+
+pub const SLEEP_INTERVAL: u64 = 60;
+pub const DATE_LENGTH: usize = 8;
+
+pub const SEPARATOR: char = '_';
